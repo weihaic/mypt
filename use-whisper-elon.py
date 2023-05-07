4bc83e1e1292e64e72731881771f593967916ebd
weihaic <55369958+weihaic@users.noreply.github.com>
2023-05-06 18:57:35 -0700

    elon talk

---
+++ use-whisper-elon.py
@@ -0,0 +1,14 @@
+
+
+from whisper_jax import FlaxWhisperPipline
+
+# instantiate pipeline
+pipeline = FlaxWhisperPipline("openai/whisper-large-v2", batch_size=16)
+
+# JIT compile the forward call - slow, but we only do once
+text = pipeline("elon.mp3",task="transcribe", return_timestamps=True)
+
+# used cached function thereafter - super fast!!
+
+
+print(text)
