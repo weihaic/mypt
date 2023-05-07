d534dc6b197605e00f46d22d39df7aea1046e91d
weihaic <55369958+weihaic@users.noreply.github.com>
2023-05-06 19:51:23 -0700

    mo

--- use-whisper-elon.py
+++ use-whisper-elon.py
@@ -3,7 +3,7 @@
 from whisper_jax import FlaxWhisperPipline
 
 # instantiate pipeline
-pipeline = FlaxWhisperPipline("openai/whisper-large-v2", batch_size=16)
+pipeline = FlaxWhisperPipline("openai/whisper-base",  dtype=jnp.bfloat16, batch_size=16)
 
 # JIT compile the forward call - slow, but we only do once
 text = pipeline("elon.mp3",task="transcribe", return_timestamps=True)
