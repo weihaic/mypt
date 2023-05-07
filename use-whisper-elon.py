ed1d79ba96cc07f33712101252a03dc58f945254
weihaic <55369958+weihaic@users.noreply.github.com>
2023-05-07 08:57:59 -0700

    Update use-whisper-elon.py

--- use-whisper-elon.py
+++ use-whisper-elon.py
@@ -1,19 +1,20 @@
 import jax.numpy as jnp
-
 from whisper_jax import FlaxWhisperPipline
+import json
 
-# instantiate pipeline
-pipeline = FlaxWhisperPipline("openai/whisper-base",  dtype=jnp.bfloat16, batch_size=16)
+# Instantiate pipeline
+pipeline = FlaxWhisperPipline("openai/whisper-base", dtype=jnp.bfloat16, batch_size=16)
 
 # JIT compile the forward call - slow, but we only do once
-text = pipeline("elon.mp3",task="transcribe", return_timestamps=True)
+text = pipeline("elon.mp3", task="transcribe", return_timestamps=True)
 
-# used cached function thereafter - super fast!!
+# Print text to the console
+print(text)
 
+# Serialize the dictionary to a JSON-formatted string
+text_str = json.dumps(text)
 
-#print(text)
+# Save the JSON string to a file
+with open("output.json", "w") as output_file:
+    output_file.write(text_str)
 
-
-# save text to a file
-with open("output.txt", "w") as output_file:
-    output_file.write(text)
